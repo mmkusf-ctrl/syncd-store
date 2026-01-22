53a1fa019f12fce6f025fe9431debcef88e6091b
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-22 01:54:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import PremiumLanding from "./pages/PremiumLanding.jsx";
 import Collection from "./pages/Collection.jsx";
 import Cart from "./pages/Cart.jsx";
 import Account from "./pages/Account.jsx";
@@ -8,7 +9,16 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      {/* Premium landing MUST be before the generic category route */}
+      <Route path="/collection/premium" element={<PremiumLanding />} />
+
+      {/* Optional: subcategory pages (necklace / ear-rings / bracelet) */}
+      <Route path="/collection/:category/:type" element={<Collection />} />
+
+      {/* Generic category listing */}
       <Route path="/collection/:category" element={<Collection />} />
+
       <Route path="/cart" element={<Cart />} />
       <Route path="/account" element={<Account />} />
       <Route path="*" element={<Navigate to="/" replace />} />
