8230ea98e6b0586fdcddfd38b8e80d7ca8f398d6
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-22 03:31:52 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,113 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import PageTransition from "./components/PageTransition.jsx";
+
 import Home from "./pages/Home.jsx";
 import Cart from "./pages/Cart.jsx";
 import Account from "./pages/Account.jsx";
 
-// Premium flow
 import PremiumLayout from "./pages/PremiumLayout.jsx";
 import PremiumLanding from "./pages/PremiumLanding.jsx";
 
-// Pearl flow
 import PearlLayout from "./pages/PearlLayout.jsx";
 import PearlLanding from "./pages/PearlLanding.jsx";
 
-// Shared sub-page grid (necklace / ear-rings / bracelet)
 import PremiumSub from "./pages/PremiumSub.jsx";
-
-// Keep your old generic page as a fallback if you want
 import Collection from "./pages/Collection.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <PageTransition>
+            <Home />
+          </PageTransition>
+        }
+      />
 
-      {/* PREMIUM (landing + subpages) */}
-      <Route path="/collection/premium" element={<PremiumLayout />}>
-        <Route index element={<PremiumLanding />} />
-        <Route path=":type" element={<PremiumSub />} />
+      {/* PREMIUM */}
+      <Route
+        path="/collection/premium"
+        element={
+          <PageTransition>
+            <PremiumLayout />
+          </PageTransition>
+        }
+      >
+        <Route
+          index
+          element={
+            <PageTransition>
+              <PremiumLanding />
+            </PageTransition>
+          }
+        />
+        <Route
+          path=":type"
+          element={
+            <PageTransition>
+              <PremiumSub />
+            </PageTransition>
+          }
+        />
       </Route>
 
-      {/* PEARL (same structure as premium) */}
-      <Route path="/collection/pearl" element={<PearlLayout />}>
-        <Route index element={<PearlLanding />} />
-        <Route path=":type" element={<PremiumSub />} />
+      {/* PEARL */}
+      <Route
+        path="/collection/pearl"
+        element={
+          <PageTransition>
+            <PearlLayout />
+          </PageTransition>
+        }
+      >
+        <Route
+          index
+          element={
+            <PageTransition>
+              <PearlLanding />
+            </PageTransition>
+          }
+        />
+        <Route
+          path=":type"
+          element={
+            <PageTransition>
+              <PremiumSub />
+            </PageTransition>
+          }
+        />
       </Route>
 
-      {/* Optional: fallback generic route */}
-      <Route path="/collection/:category" element={<Collection />} />
+      {/* OPTIONAL FALLBACK */}
+      <Route
+        path="/collection/:category"
+        element={
+          <PageTransition>
+            <Collection />
+          </PageTransition>
+        }
+      />
 
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/account" element={<Account />} />
+      <Route
+        path="/cart"
+        element={
+          <PageTransition>
+            <Cart />
+          </PageTransition>
+        }
+      />
+
+      <Route
+        path="/account"
+        element={
+          <PageTransition>
+            <Account />
+          </PageTransition>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
