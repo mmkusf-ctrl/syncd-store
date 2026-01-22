92b3268d5f39866c81d432b77de8ae4feab09143
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-22 04:13:52 -0500

    Update PageTransition.jsx

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -15,3 +15,19 @@ export default function PageTransition({ children }) {
     </div>
   );
 }
+
+import { useEffect, useState } from "react";
+
+export default function PageTransition({ children }) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
+  return (
+    <div className={`page ${show ? "page-show" : ""}`}>
+      {children}
+    </div>
+  );
+}
