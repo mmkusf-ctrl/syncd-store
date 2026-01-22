e6b583c349c53b5fd1f1669ee7e5fe13252599ac
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-21 23:54:44 -0500

    Create account.jsx

---
+++ src/pages/account.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+export default function Account() {
+  return (
+    <div style={{ padding: 22, fontFamily: "Arial, sans-serif" }}>
+      <Link to="/">← Home</Link>
+      <h1>Account</h1>
+      <p>Login/Register will be added next.</p>
+    </div>
+  );
+}
