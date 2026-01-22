2e44672807dbe0e0f6da6940d87e908ae3556d79
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-22 03:46:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+import { CartProvider } from "./context/CartContext.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <CartProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </CartProvider>
   </React.StrictMode>
 );
