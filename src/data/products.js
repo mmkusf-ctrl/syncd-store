daf2b6dfad9e7d3648ccd29083dc2e0330949fe1
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-21 23:50:54 -0500

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+export const products = [
+  { id: 1, name: "Premium Necklace 01", price: 49.99, category: "premium" },
+  { id: 2, name: "Premium Necklace 02", price: 59.99, category: "premium" },
+  { id: 3, name: "Premium Necklace 03", price: 39.99, category: "premium" },
+  { id: 4, name: "Premium Necklace 04", price: 44.99, category: "premium" },
+  { id: 5, name: "Premium Necklace 05", price: 69.99, category: "premium" },
+  { id: 6, name: "Premium Necklace 06", price: 74.99, category: "premium" },
+  { id: 7, name: "Premium Necklace 07", price: 29.99, category: "premium" },
+  { id: 8, name: "Premium Necklace 08", price: 34.99, category: "premium" },
+  { id: 9, name: "Premium Necklace 09", price: 89.99, category: "premium" },
+  { id: 10, name: "Premium Necklace 10", price: 99.99, category: "premium" },
+  { id: 11, name: "Premium Necklace 11", price: 109.99, category: "premium" },
+
+  { id: 101, name: "Pearl Necklace 01", price: 79.99, category: "pearl" },
+  { id: 102, name: "Pearl Necklace 02", price: 84.99, category: "pearl" },
+  { id: 103, name: "Pearl Necklace 03", price: 74.99, category: "pearl" },
+  { id: 104, name: "Pearl Necklace 04", price: 89.99, category: "pearl" },
+  { id: 105, name: "Pearl Necklace 05", price: 94.99, category: "pearl" },
+  { id: 106, name: "Pearl Necklace 06", price: 64.99, category: "pearl" },
+  { id: 107, name: "Pearl Necklace 07", price: 69.99, category: "pearl" },
+  { id: 108, name: "Pearl Necklace 08", price: 59.99, category: "pearl" },
+  { id: 109, name: "Pearl Necklace 09", price: 99.99, category: "pearl" },
+  { id: 110, name: "Pearl Necklace 10", price: 109.99, category: "pearl" },
+  { id: 111, name: "Pearl Necklace 11", price: 119.99, category: "pearl" }
+];
