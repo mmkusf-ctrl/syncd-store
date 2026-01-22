1d246444caf73f8755c3e0c00e31ffc7237e43a9
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-21 23:54:19 -0500

    Create cart.jsx

---
+++ src/pages/cart.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+export default function Cart() {
+  return (
+    <div style={{ padding: 22, fontFamily: "Arial, sans-serif" }}>
+      <Link to="/">← Home</Link>
+      <h1>Cart</h1>
+      <p>Cart functionality will be added next.</p>
+    </div>
+  );
+}
