edb074e43f83b9d0762d0ae322d76aff2f9e098a
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-01-22 20:21:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Cart from "./pages/Cart.jsx";
-import Account from "./pages/Account.jsx";
-
-import CollectionLanding from "./pages/CollectionLanding.jsx";
-import CollectionSub from "./pages/CollectionSub.jsx";
-import AnimatedRoutes from "./components/AnimatedRoutes.jsx";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import CollectionSub from "./pages/CollectionSub";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
 
 export default function App() {
   return (
     <HashRouter>
-      <AnimatedRoutes>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-          {/* Collection landing (Premium/Pearl) */}
-          <Route path="/collection/:collection" element={<CollectionLanding />} />
+        <Route path="/collection/:type" element={<Collection />} />
+        <Route path="/collection/:type/:sub" element={<CollectionSub />} />
 
-          {/* Sub pages */}
-          <Route path="/collection/:collection/:sub" element={<CollectionSub />} />
-
-          {/* Legacy redirect: ear-rings -> earrings */}
-          <Route
-            path="/collection/:collection/ear-rings"
-            element={<LegacyRedirect toSub="earrings" />}
-          />
-
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/account" element={<Account />} />
-
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AnimatedRoutes>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </HashRouter>
   );
-}
-
-function LegacyRedirect({ toSub }) {
-  // keep same collection param, redirect sub
-  const { collection } = require("react-router-dom").useParams();
-  return <Navigate to={`/collection/${collection}/${toSub}`} replace />;
 }
